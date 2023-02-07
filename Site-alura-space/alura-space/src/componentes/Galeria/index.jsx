2c88d7f284eafc317b9c1614656ef89d9e588a37
gildairmoreira <104792309+gildairmoreira@users.noreply.github.com>
2023-02-07 08:41:03 -0300

    USE STATE

--- Site-alura-space/alura-space/src/componentes/Galeria/index.jsx
+++ Site-alura-space/alura-space/src/componentes/Galeria/index.jsx
@@ -1,16 +1,17 @@
-import React from 'react'
+import React, { useState } from 'react'
 import Tags from '../Tags'
 import styles from './galeria.module.scss'
 import fotos from './fotos.json'
 import Cards from './Cards'
 
-export default function Galeria()
-{
+export default function Galeria() {
+  const [itens, setItens] = useState(fotos)
+  const tags = [...new Set(fotos.map((valor) => valor.tag))]
   return (
     <section className={styles.galeria}>
       <h2>Navegue pela Galeria</h2>
-      <Tags />
-      <Cards itens={fotos} styles={styles} />
+      <Tags tags={tags} />
+      <Cards itens={itens} styles={styles} />
     </section>
   )
 }
--- Site-alura-space/alura-space/src/componentes/Tags/index.jsx
+++ Site-alura-space/alura-space/src/componentes/Tags/index.jsx
@@ -1,16 +1,18 @@
 import React from 'react'
 import styles from './Tags.module.scss'
 
-export default function Tags() {
+export default function Tags(tags)
+{
   return (
     <div className={styles.tags}>
       <p>Busque por tags:</p>
       <ul className={styles.tags__lista}>
-            <li>Estrelas</li>
-            <li>Galáxias</li>
-            <li>Lua</li>
-            <li>Planetas</li>
-        </ul>
+        {tags.map((tag) => {
+          return (
+            <li key={tag}>{tag}</li>
+          )
+        })}
+      </ul>
     </div>
   )
 }
