e9f5425cb3cc670a0434180ea6ca628f2d8cd1ef
gildairmoreira <104792309+gildairmoreira@users.noreply.github.com>
2023-02-07 08:42:35 -0300

    Update index.jsx

--- Site-alura-space/alura-space/src/componentes/Galeria/index.jsx
+++ Site-alura-space/alura-space/src/componentes/Galeria/index.jsx
@@ -5,8 +5,10 @@ import fotos from './fotos.json'
 import Cards from './Cards'
 
 export default function Galeria() {
+
   const [itens, setItens] = useState(fotos)
   const tags = [...new Set(fotos.map((valor) => valor.tag))]
+
   return (
     <section className={styles.galeria}>
       <h2>Navegue pela Galeria</h2>
