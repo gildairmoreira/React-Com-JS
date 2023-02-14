435069df58ef58261f119fac0552af74ea4c912e
gildairmoreira <104792309+gildairmoreira@users.noreply.github.com>
2023-02-13 21:03:20 -0300

    cards

--- site-routes/messi/src/routes.js
+++ site-routes/messi/src/routes.js
@@ -1,18 +1,25 @@
+import PaginaPadrao from "componentes/PaginaPadrao";
+import Rodape from "componentes/Rodape";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Menu from "./componentes/Menu";
-import Inicio from './paginas/Inicio';
-import SobreMim from './paginas/SobreMim';
+import Inicio from "./paginas/Inicio";
+import SobreMim from "./paginas/SobreMim";
 
 function AppRoutes() {
   return (
     <BrowserRouter>
-      <Menu/>
+      <Menu />
 
       <Routes>
-        <Route path="/" element={<Inicio />} />
-        <Route path="/sobremim" element={<SobreMim />} />
+        <Route path="/" element={<PaginaPadrao />}>
+          <Route path="/" element={<Inicio />} />
+          <Route path="/sobremim" element={<SobreMim />} />
+        </Route>
+
         <Route path="*" element={<div>Página não encontrada</div>} />
       </Routes>
+
+      <Rodape />
     </BrowserRouter>
   );
 }
