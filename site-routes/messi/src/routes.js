e93b2f78aeaeddcbd1e630982e379a1b61e59cf8
gildairmoreira <104792309+gildairmoreira@users.noreply.github.com>
2023-02-14 18:53:28 -0300

    projeto finalizado entre aspass

--- site-routes/messi/src/routes.js
+++ site-routes/messi/src/routes.js
@@ -1,5 +1,8 @@
 import PaginaPadrao from "componentes/PaginaPadrao";
 import Rodape from "componentes/Rodape";
+import ScrollToTop from "componentes/ScrollToTop";
+import NaoEncontrada from "paginas/NaoEncontrada";
+import Post from "paginas/Post";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Menu from "./componentes/Menu";
 import Inicio from "./paginas/Inicio";
@@ -8,15 +11,17 @@ import SobreMim from "./paginas/SobreMim";
 function AppRoutes() {
   return (
     <BrowserRouter>
+      <ScrollToTop />
       <Menu />
 
       <Routes>
         <Route path="/" element={<PaginaPadrao />}>
-          <Route path="/" element={<Inicio />} />
-          <Route path="/sobremim" element={<SobreMim />} />
+          <Route index element={<Inicio />} />
+          <Route path="sobremim" element={<SobreMim />} />
         </Route>
+        <Route path="posts/:id/*" element={<Post />} />
 
-        <Route path="*" element={<div>Página não encontrada</div>} />
+        <Route path="*" element={<NaoEncontrada />} />
       </Routes>
 
       <Rodape />
