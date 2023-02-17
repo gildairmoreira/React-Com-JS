2bd1328b7cfdaf960dc3afcf32a941b636196154
gildairmoreira <104792309+gildairmoreira@users.noreply.github.com>
2023-02-17 09:49:27 -0300

    componentização

---
+++ site-caixa-eletronico/src/index.js
@@ -0,0 +1,41 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import './index.css';
+import { createTheme, StylesProvider, ThemeProvider } from '@material-ui/core';
+import Login from './pages/Login';
+/* import App from './App'; */
+
+const theme = createTheme({
+  palette: {
+    primary: {
+      main: "#2A9F85",
+    },
+    secondary: {
+      main: "#FF7070",
+    },
+  },
+});
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
+  <React.StrictMode>
+    <StylesProvider injectFirst>
+      <ThemeProvider theme={theme}>
+        <Login />
+      </ThemeProvider>
+    </StylesProvider>
+  </React.StrictMode>
+);
+
+
+ // JEITO ANTIGO
+/* ReactDOM.render(
+  <React.StrictMode>
+    <StylesProvider injectFirst>
+      <ThemeProvider theme={theme}>
+        <Login />
+      </ThemeProvider>
+    </StylesProvider>
+  </React.StrictMode>,
+  document.getElementById("root")
+); */
