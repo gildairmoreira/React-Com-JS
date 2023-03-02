6338908ea7482ceb0d68637878d65de159f05434
gildairmoreira <104792309+gildairmoreira@users.noreply.github.com>
2023-03-02 09:00:23 -0300

    90%

--- site-cinema/src/pages/Favoritos/index.jsx
+++ site-cinema/src/pages/Favoritos/index.jsx
@@ -1,20 +1,20 @@
 import Banner from "components/Banner";
 import Card from "components/Card";
 import Titulo from "components/Titulo";
+import { useFavoritoContext } from "contextos/Favoritos";
 import React from "react";
 import styles from "./Favoritos.module.scss";
 
 export default function Favoritos() {
+  const {favorito} = useFavoritoContext()
   return (
     <>
       <Banner imagem="favoritos"/>
       <Titulo><h1>Meus Favoritos</h1></Titulo>
       <section className={styles.container}>
-        <Card
-          id="2"
-          titulo="Gato bonifácio"
-          capa={"https://thecatapi.com/api/images/get?format=src&type=png"}
-        />
+        {favorito.map((fav) => {
+          return <Card {...fav} key={fav.id} />
+        })}
       </section>
     </>
   );
